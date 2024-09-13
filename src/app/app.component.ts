0a9f8dafa0ad7636e3afc72822a87cf557e41ce8
Geovane Araujo <geovane-araujo@outlook.com>
2024-09-13 14:44:14 -0300

    starter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { Interceptor } from './config/interceptor/interceptor';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    BrowserModule,
+    HttpClientModule,
+    ToastModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: Interceptor,
+      multi: true
+    },
+    MessageService
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './security/login/login.component';
+import { publicGuard } from './security/guards/public.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "login", component: LoginComponent, pathMatch: "full", canActivate: [publicGuard] },
+];
