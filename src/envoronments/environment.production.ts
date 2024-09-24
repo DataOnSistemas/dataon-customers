d7929c07426931cd3dd0c1a81dfb11b59a11cffc
Geovane Araujo <geovane-araujo@outlook.com>
2024-09-24 17:03:37 -0300

    teste

--- src/envoronments/environment.production.ts
+++ src/envoronments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://apidev.dataon.com.br/api',
+    apiUrl: 'https://apidev.dataon.com.br/v2/api',
     basicAuth: 'Basic ZGF0YW9uOkRhdGFPbkFQSUAj',
 };
